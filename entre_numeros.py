082dc13d74cc234c65aa58424374422e474ce01f
Jesus peña <jenrique.ps@gmail.com>
2014-07-28 22:25:41 -0500

    comentarios

--- entre_numeros.py
+++ entre_numeros.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 *-*
 __author__ = 'jenriqueps'
+# Problema
 """introducir dos numeros por teclado. Imprimir los numeros que hay entre ellos
 comenzando por el mas pequeno. Contar cuantos hay y cuantos de ellos son
 pares. Calcular la suma de los pares"""
+#recibiendo numeros pares
 numero1 = input("ingrese numero :")
 numero2 = input("ingrese siguiente numero:")
 
@@ -10,18 +12,21 @@ lista = [numero1, numero2]
 if numero1 == numero2:
     print("Los dos son iguales no hay numeros enteros entre ellos.")
 else:
-    num_max = max(lista)
-    num_min1 = min(lista)
-    num_min = num_min1 + 1
-    rango_entre_numero = range((num_min), int(num_max))
-    cantidad_de_numeros = len(rango_entre_numero)
+    num_max = max(lista)    # obteniendo el numero mayor
+    num_min1 = min(lista)   # obteniendo el numero menor
+    num_min = num_min1 + 1  # aumentando  mas 1 al numero menor par eleminarlo
+                            # del rango
+    rango_entre_numero = range((num_min), (num_max))    # obteniendo rango
+    cantidad_de_numeros = len(rango_entre_numero)   # cantidad de numeros
     print("cantidad de numeros entre {} y {} es: {} " ).format(num_min1,
                                                                num_max,
                                                                cantidad_de_numeros)
-    numeros_pares = []
-    suma_pares = 0
+    numeros_pares = []      # lista de numeros pares
+    suma_pares = 0          # variable almacena la suma de los numeros pares
 
     print('Los numeros Pares son:')
+    # declarando bucle para obtener numeros pares y añadirlos a variable
+    # numeros_pares
     for par in rango_entre_numero:
         if par % 2 == 0:
             numeros_pares.append(par)
--- nun_primo.py
+++ nun_primo.py
@@ -29,5 +29,3 @@ def primo(num):
 
 primo(5000)
 
-
-
